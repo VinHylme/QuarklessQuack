8b0a4c2166b13ca66d5c046bca5cc39bd83ab576
vinhylme <info.quantabit@gmail.com>
2019-11-26 17:06:57 +0000

    deleted quarkless.mediaAnalyser as is old

--- Quarkless.MediaAnalyser/MediaAnalyserOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Quarkless.MediaAnalyser
-{
-	public class MediaAnalyserOptions
-	{
-		public string TempVideoPath { get; set; }
-		public string TempImagePath { get; set; }
-		public string FfmpegEnginePath { get; set; }
-	}
-}
